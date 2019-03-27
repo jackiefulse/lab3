d0d7491ec0eaa05a55b1d9d50208f3e5d5a8be39
jackiefulse <33031445+jackiefulse@users.noreply.github.com>
2019-03-26 23:10:27 -0400

    Add files via upload

---
+++ Detail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+export default class Detail extends React.Component {
+    static navigationOptions = {
+      header: null,
+    }
+    render(){
+    return <Text>Hello World</Text>
+    }
+}
